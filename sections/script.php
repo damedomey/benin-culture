88198ff344d48a8408d0d010fe2df081a8477cc0
AMEDOMEY Romeo David <daroshatheldeep@gmail.com>
2021-09-08 17:07:28 +0100

    feat: Réalisation du footer et reprise de la carousel

--- sections/script.php
+++ sections/script.php
@@ -1,3 +1,4 @@
 <!-- Script -->
 <script src="assets/jquery/dist/jquery.min.js"></script>
 <script src="assets/bootstrap/dist/js/bootstrap.min.js"></script>
+<script src="assets/bootstrap/dist/js/bootstrap2.bundle.min.js"></script>
--- sections/stylesheets.php
+++ sections/stylesheets.php
@@ -1,4 +1,5 @@
-
-    <!-- Stylesheets -->
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/bootstrap/dist/css/bootstrap.min.css">
+<!-- Stylesheets -->
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="assets/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="assets/css/animate.min.css">
+<link rel="stylesheet" href="assets/icofont/icofont.min.css">
